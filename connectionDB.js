64e9473d213cbc106d8baf5cfc18a4f8e91a0d19
ShiljaBabu <shiljababu78@gmail.com>
2025-11-15 17:54:08 +0530

    atlas connected

--- connectionDB.js
+++ connectionDB.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose"
 import dotenv from 'dotenv'
+
 dotenv.config()
 
 const connectionstring = process.env.DBCONNECTIONSTRING
-const connectDb = async ()=>{
-    try{
 
+const connectDb = async () => {
+    try {
         await mongoose.connect(connectionstring)
-        console.log("MongoDB connected successfully");
-        
-    }catch(err){
-        console.log("MongoDB connection failed!!",err.message)
+        console.log("MongoDB connected successfully")
+    } catch (err) {
+        console.log("MongoDB connection failed!!", err.message)
     }
 }
+
 export default connectDb
